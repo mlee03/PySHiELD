db90752c3f971e32dc286d26090a488adeeadf66
Frank Malatino <frank.malatino@noaa.gov>
2024-07-01 14:40:01 -0400

    Removing package directory

--- pySHiELD/stencils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .microphysics import Microphysics, MicrophysicsState
-
-
-"""
-Microphysics: GFS Cloud Microphysics class
-MicrophysicsState: Class containing the state for the GFS Cloud Microphysics
-"""
